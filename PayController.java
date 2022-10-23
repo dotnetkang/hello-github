b35ed193439f8e1ae54ac0dc6df9d894a123fa80
KANG-NB\kang-NB <dotnet@daum.net>
2022-10-23 21:25:01 +0900

    PG 결제 클래스
     PayController 개발

---
+++ PayController.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+@Controller
+public class PayController {
+    
+    @RequestMapping(value = "payAfter.do")
+    public String payAfter(modelMap model, HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+    }
+}
